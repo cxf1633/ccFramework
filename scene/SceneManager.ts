7255c2ad0b401c0e5e1b7945e1d96943d4b433c6
chenxufeng <cxf1633@gmail.com>
2026-07-24 11:11:36 +0800

    场景加载支持异步进度

--- scene/SceneManager.ts
+++ scene/SceneManager.ts
@@ -1,6 +1,8 @@
 import { director } from "cc";
 import { ResManager } from "../res/ResManager";
 
+export type ScenePreloadProgressCallback = (finished: number, total: number) => void;
+
 export class SceneManager {
     public constructor(private readonly res: ResManager) { }
 
@@ -26,18 +28,28 @@ export class SceneManager {
     //     await this.loadScene(sceneName);
     // }
 
-    public async preloadScene(bundleName: string, sceneName: string, options?: Record<string, any>): Promise<void> {
+    public async preloadScene(
+        bundleName: string,
+        sceneName: string,
+        options?: Record<string, any>,
+        onProgress?: ScenePreloadProgressCallback,
+    ): Promise<void> {
         const bundle = await this.res.ensureBundle(bundleName, options);
 
         return new Promise((resolve, reject) => {
-            bundle.preloadScene(sceneName, (err) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
+            bundle.preloadScene(
+                sceneName,
+                null,
+                (finished, total) => onProgress?.(finished, total),
+                (err) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
 
-                resolve();
-            });
+                    resolve();
+                },
+            );
         });
     }
 
